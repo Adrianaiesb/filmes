2492376e1c1f8ba85ef1161ffd9ee211554c1f31
Adriana <iesbadriana@gmail.com>
2023-04-20 20:34:34 -0300

    Novo

--- pages/filmes/[id].js
+++ pages/filmes/[id].js
@@ -1,12 +1,32 @@
 import Pagina from '@/components/Pagina'
 import apiFilmes from '@/services/apiFilmes'
 import React from 'react'
+import { Card, Col, Row } from 'react-bootstrap'
 
-const Detalhes = ({ filme }) => {
-
+const Detalhes = ({filme}) => {
     return (
-        <Pagina titulo={props.filme.title}>
-            <div>Detalhes</div>
+        <Pagina titulo={filme.title}>
+            <Row>
+                <Col md={3}>
+                    <Card.Img variant="top" src={"https://image.tmdb.org/t/p/w500" + filme.poster_path} />
+                </Col>
+
+                <Col md={9}>
+                    <p><strong>Orçamento: </strong>{filme.budget}</p>
+                    <p><strong>Data de Lançamento: </strong>{filme.release_date}</p>
+                    <p><strong>Duração: </strong>{filme.runtime} min</p>
+                    <p><strong>Nota: </strong>{filme.vote_average}</p>
+                    <div>
+                        <strong>Gêneros: </strong>
+                        <ul>
+                            {filme.genres.map(item=> (
+                                <li>{item.name}</li>
+                            ))}
+                        </ul>
+                    </div>
+                    <p>{filme.overview}</p>
+                </Col>
+            </Row>
         </Pagina>
     )
 }
